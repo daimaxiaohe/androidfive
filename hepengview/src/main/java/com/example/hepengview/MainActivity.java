c4ce15969522d04d5228af92dc49e7c47f646614
はすてすゃの <1364182300@qq.com>
2019-01-02 21:24:40 +0800

    自定义view

--- hepengview/src/main/java/com/example/hepengview/MainActivity.java
+++ hepengview/src/main/java/com/example/hepengview/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myView = findViewById(R.id.myview);
-
+        myView.invalidate();
     }
 }
